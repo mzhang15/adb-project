82b30bd0faaa613d10daad007bc850a4959685e1
Mengyang <mengyzhang87@gmail.com>
2020-11-25 17:12:32 -0500

    initial commit

---
+++ project.py
@@ -0,0 +1,46 @@
+class TM:
+	# initialize TM: start time, end time, is site up array, is read-only array, waiting commands, wait for
+	def __init__(self):
+		self.start_time
+		self.end_time
+		self.is_site_up
+		self.is_read_only
+		self.waiting
+		self.waits_for
+
+
+class DM:
+	def __init__(self):
+		self.curr_vals
+		self.commit_vals
+		self.lock_table
+	
+	# initialize variables' values
+	def initialize():
+
+
+
+# read a line from input file and determine what command it is
+def read_in(line):
+
+
+
+def begin(transaction):
+
+
+def read(transaction, val):
+
+def read_only(transaction, var):
+
+def write(transaction, var, value):
+
+def end(t):
+
+
+def fail(site):
+
+def recover(site):
+
+def dump():
+
+def querystat():
