3b6d4eea01c0d5b47e72aeca4ddfe12ff04f0622
Mengyang <mengyzhang87@gmail.com>
2020-11-26 12:59:22 -0500

    created DB class & added querystate method

--- project.py
+++ project.py
@@ -1,68 +1,98 @@
-class TM:
-	# initialize TM: start time, end time, is site up array, is read-only array, waiting commands, wait for
+
+class DB:
+
 	def __init__(self):
-		self.start_time = {} # dictionary with key = transaction no, value = start time
-		self.end_time = {}
-		self.status = [1 for x in range(10)] # 0 - down 1 - up normally 2 - just recovered
-		self.is_read_only = [False] * 10
-		self.waiting = [] # accumulate waiting command
-		self.waits_for = [] # wait-for edges, used for deadlock detection
-		self.accessed_sites = {} # key: transaction, value: list of sites it has accessed
+		self.tm = self.TM()
+		self.num_of_sites = 10
+		self.sites = [self.DM() for i in range(self.num_of_sites + 1)]
 
-	# def deadlock_detect():
+	class TM:
+		
+		# status constants
+		DOWN = 0
+		UP = 1
+		RECOVER = 2
 
+		# initialize TM: start time, end time, is site up array, is read-only array, waiting commands, wait for
+		def __init__(self):
+			self.num_of_sites = 10
+			self.start_time = {} # dictionary with key = transaction no, value = start time
+			self.end_time = {}
+			self.status = [self.UP for x in range(self.num_of_sites + 1)] # 0 - down 1 - up normally 2 - just recovered
+			self.is_read_only = [False] * (self.num_of_sites + 1)
+			self.waiting = [] # accumulate waiting command
+			self.waits_for = [] # wait-for edges, used for deadlock detection
+			self.accessed_sites = {} # key: transaction, value: list of sites it has accessed
 
-class DM:
-	def __init__(self):
-		self.curr_vals = {} # is a map: has key means try to write to it, when site is down, erase its value but leave the key. 
-		self.commit_vals = [x * 10 for x in range(11)]
-		self.lock_table = {}
+		# def deadlock_detect():
+
+		def print_state(self):
+			print("status: ", self.status)
 	
-	# initialize variables' values
-	# def initialize():
+	class DM:
 
-# read a line from input file and determine what command it is
-# def read_in(line):
+		def __init__(self):
+			self.num_of_var = 20
+			self.curr_vals = {} # is a map: has key means try to write to it, when site is down, erase its value but leave the key. 
+			self.commit_vals = [x * 10 for x in range(self.num_of_var + 1)]
+			self.lock_table = {}
+		
+		def print_state(self):
+			print("commit values: ", self.commit_vals)
+		# initialize variables' values
+		# def initialize():
+
+	# read a line from input file and determine what command it is
+	# def read_in(line):
 
 
-# def begin(transaction, time):
-	# just record its begin time
+	# def begin(transaction, time):
+		# just record its begin time
 
-# return the value
-# def read(transaction, val):
+	# return the value
+	# def read(transaction, val):
 
-# def read_only(transaction, var):
+	# def read_only(transaction, var):
 
-# def write(transaction, var, value):
+	# def write(transaction, var, value):
 
-# return commit or abort
-# def end(t):
-	# release locks
+	# return commit or abort
+	# def end(t):
+		# release locks
 
-	# check if all sites t has accessed can commit, which means assign curr_vals to commit_vals
-	# (may not need to do this since we already abort the transaction in fail instruction)
+		# check if all sites t has accessed can commit, which means assign curr_vals to commit_vals
+		# (may not need to do this since we already abort the transaction in fail instruction)
 
-	# commit values: assign curr_vals to commit_vals
+		# commit values: assign curr_vals to commit_vals
 
-# def fail(site):
-	# erase lock table + curr_vals?
+	# def fail(site):
+		# erase lock table + curr_vals?
 
-	# check if a transactionn has accessed this site, if so, abort it right away
+		# check if a transactionn has accessed this site, if so, abort it right away
 
-# def recover(site):
+	# def recover(site):
 
-# def dump():
+	# def dump():
 
-# def querystat():
+	def querystate(self):
+		print("Transaction Manager State:")
+		self.tm.print_state()
+		print("Sites: ")
+		for s in self.sites:
+			s.print_state()
 
 
 def main():
-	# setup 1 TM and 10 DM
+	# create a DB
+	db = DB()
 
 	# read a file line by line
 	f = open("input.txt", "r")
 	for x in f:
 		print(x)
+
+	db.querystate()
+
 
 if __name__ == "__main__":
     main()
